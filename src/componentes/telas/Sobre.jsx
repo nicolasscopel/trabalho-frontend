cd971bdd382c9c6c4a0d27707613584a8c2f9a30
nicolasscopel <nicolaspscopel@gmail.com>
2025-05-08 17:51:52 -0300

    Final

--- src/componentes/telas/Sobre.jsx
+++ src/componentes/telas/Sobre.jsx
@@ -2,7 +2,11 @@ const Sobre = () => (
     <div>
         <h1>Sobre o Trabalho</h1>
         <h3>Sistema desenvolvido na Disciplina de Programação para Web para atender uma locadora de veículos.</h3>
-       
+        <div>
+        <img src="/diagrama.png" alt="Diagrama UML" style={{display:"block", margin:"0 auto", width: '100%', maxWidth: '800px', padding:"50px" }} />
+        <h3 style={{ textAlign: 'center', fontWeight:"bold" }}>Diagrama de Classes do Sistema</h3>
+        </div>
+        
     </div>
 );
 export default Sobre;
